5dfa7c3b2c12d55e3214bcd18b52b2c61807795b
Janaki Ram Emani <98529167+JANAKIRAMEMANI@users.noreply.github.com>
2022-01-31 11:17:09 +0530

    Create PEN_MUX_TB.v

---
+++ PEN_MUX_TB.v
@@ -0,0 +1,22 @@
+module fmys_4x2msbpe_tb;
+    reg f,m,y,s;
+    wire y1,y0;
+
+    fmys_4x2msbpe BHALLA (y1,y0,f,m,y,s);
+
+
+    initial begin
+        $dumpfile("dump.vcd"); 
+        $dumpvars;
+      end
+  
+  initial 
+	begin
+      for(int j=0; j<=15; j++) //repeat(15)
+			begin
+              {f,m,y,s}= j;
+			#1;
+			$display(f,m,y,s,":",y1,y0);
+			end
+	end
+endmodule
